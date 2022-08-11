d680e52b0097492886d8715d24cf385d9f6ba63c
Nana_77 <nana77amery@gmail.com>
2022-08-11 11:04:16 +0900

    ゲームマネージャーの調整とゴールを出すカウントの追加

--- Assets/Okuyama/Stage.cs
+++ Assets/Okuyama/Stage.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class Stage : MonoBehaviour
 {
+    [SerializeField] int _GoleInstans = 10;
+    [SerializeField] float _backGroundSpeed = 0.2f;
     [SerializeField] float _speedMax = 5;
     [SerializeField] float _speed = 5;
     [SerializeField] float _minusSpeed = 3;
+    [SerializeField] EnemyGenerator _enemyGenerator;
     [SerializeField] SpriteRenderer[] _nigthStage;
     [SerializeField] SpriteRenderer[] _brakingDownStage;
     [Tooltip("スタート位置")]private Vector2 startPos;
     [Tooltip("背景幅")] float repeatWidth;
     bool _mainusBool = false;
-    //SpriteRenderer _spriteRenderer;
-    
+    int _goleCount = 0;
+
     void Start()
     {
         startPos = transform.position;
@@ -41,6 +44,8 @@ public class Stage : MonoBehaviour
         if (transform.position.x < startPos.x - repeatWidth)
         {
             transform.position = startPos;
+            _goleCount++;
+            //if(_goleCount == _GoleInstans) { _enemyGenerator.GoleGeneration()}
         }
         transform.Translate(Vector3.left * _speed * Time.deltaTime, Space.World);
     }
@@ -49,8 +54,9 @@ public class Stage : MonoBehaviour
         foreach (SpriteRenderer childTransform in _nigthStage)
         {
             float color_a = 0;
-            color_a -= Time.deltaTime;
+            color_a -= _backGroundSpeed * Time.deltaTime;
             childTransform.color += new Color(0, 0, 0, color_a);
+            if(color_a < -120) { childTransform.gameObject.SetActive(false); }
         }
         if (transform.position.x < startPos.x - repeatWidth)
         {
@@ -63,8 +69,9 @@ public class Stage : MonoBehaviour
         foreach (SpriteRenderer childTransform in _brakingDownStage)
         {
             float color_a = 0;
-            color_a -= Time.deltaTime;
+            color_a -= _backGroundSpeed * Time.deltaTime;
             childTransform.color += new Color(0, 0, 0, color_a);
+            if (color_a < -120) { childTransform.gameObject.SetActive(false); }
         }
         if (transform.position.x < startPos.x - repeatWidth)
         {
